6080f7da974656a7bd1b5eae4af1d698e46a874f
zahranemehdi <zahranemehdi@gmail.com>
2017-10-27 11:27:17 +0100

    Add MvcMovieContect Class wich extends from the DbContext class, plus overrinding the default constructor, plus the DbSet property

---
+++ Models/MvcMovieContext.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+namespace MvcMovie.Models
+{
+    public class MvcMovieContext : DbContext
+    {
+            public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+                :base(options)
+            {
+
+            }
+            public DbSet<MvcMovie.Models.Movie> Movie { get; set; }
+    }
+}
